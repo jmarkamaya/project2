b96228f071723dadd9f245c621e9adaeee5b05f1
Jon Amaya <jonamaya@Jons-MacBook-Air.local>
2023-09-05 17:58:40 -0700

    push

--- Employee-Manager-Tracker-Project-main/utils/checkAuth.js
+++
@@ -1,9 +0,0 @@
-function checkAuthenticated(req, res, next) {
-    if (req.session && req.session.user) {
-        return next();
-    }
-
-    res.redirect('/login');
-}
-
-module.exports = { checkAuthenticated };
